e56ff73072ba3110ccb0c943abf4200b5226e148
isabellavieira57@gmail.com <isabellavieira57@gmail.com>
2016-04-18 23:17:39 -0300

    Add scripts para gerar calculo spearman... Erro no script R do louvain quando arquivo esta vazio

--- pagerank/experimentos.sh
+++ pagerank/experimentos.sh
@@ -1,52 +1,52 @@
-# Oraculo: top 10 (sem ordenacao feita pelos desenvolvedores)
+# Oraculo: top 10 (deve estar ordenado pelos desenvolvedores de 1(mais importante) a 10(menos importante))
 
 echo
 echo
 echo "Executando testes AngularJS..."
 echo "Angular_2895.txt"
-python main.py -c entradas/angular_2895.txt 28,17,18,20,23,27,33,2,10,13
+python main.py -c entradas/angular_2895.txt 17,13,18,20,23,27,28,33,10,2
 echo "Angular_734.txt"
-python main.py -c entradas/angular_734.txt 4,5,36,2,12,22,25,26,39,8
+python main.py -c entradas/angular_734.txt 5,8,4,12,39,2,25,22,26,36
 echo "Angular_1412.txt"
-python main.py -c entradas/angular_1412.txt 3,4,7,15,29,6,10,12,16,24
+python main.py -c entradas/angular_1412.txt 6,15,24,29,12,4,3,16,10,7
 echo "Angular_583.txt"
-python main.py -c entradas/angular_583.txt 2,8,10,11,13,18,7,12,16,38   
+python main.py -c entradas/angular_583.txt 11,13,12,2,18,16,8,10,38,7
 echo "Angular_5160.txt"
-python main.py -c entradas/angular_5160.txt 5,6,2,3,7,9,14,15,4,8
+python main.py -c entradas/angular_5160.txt 2,3,5,6,15,7,8,9,14,4
 echo "Fim teste angularjs"
 echo
 echo
 
 echo "Executando testes jQuery..."
 echo "jquery_2145.txt"
-python main.py -c entradas/jquery_2145.txt 14,2,9,12,13,15,16,17,23,24
+python main.py -c entradas/jquery_2145.txt 9,2,24,13,16,15,12,14,17,23
 echo "jquery_2310.txt"
-python main.py -c entradas/jquery_2310.txt 4,30,37,5,10,13,14,17,19,38
+python main.py -c entradas/jquery_2310.txt 37,5,10,38,30,14,13,4,19,17
 echo "jquery_2199.txt"
-python main.py -c entradas/jquery_2199.txt 7,9,12,15,16,19,20,3,4,6
+python main.py -c entradas/jquery_2199.txt 3,7,6,15,20,16,9,12,19,4
 echo "jquery_1692.txt"
-python main.py -c entradas/jquery_1692.txt 6,8,10,11,14,17,21,26,28,4
+python main.py -c entradas/jquery_1692.txt 21,11,8,10,14,28,17,6,4,26
 echo "jquery_2321.txt"
-python main.py -c entradas/jquery_2321.txt 2,17,21,3,4,5,8,10,11,12
+python main.py -c entradas/jquery_2321.txt 2,3,4,5,17,21,11,10,8,12
 echo "Fim teste jquery"
 echo
 echo
 
 echo "Executando testes Bootstrap..."
 echo "bootstrap_341.txt"
-python main.py -c entradas/bootstrap_341.txt 3,21,29,31,35,5,6,9,24,30
+python main.py -c entradas/bootstrap_341.txt 30,24,6,9,35,21,31,5,29,3
 echo "bootstrap_1235.txt"
-python main.py -c entradas/bootstrap_1235.txt 2,5,11,18,19,30,3,7,22,26
+python main.py -c entradas/bootstrap_1235.txt 19,18,11,5,22,26,30,2,7,3
 echo "bootstrap_931.txt"
-python main.py -c entradas/bootstrap_931.txt 9,13,14,17,21,24,25,26,28,34
+python main.py -c entradas/bootstrap_931.txt 28,24,25,26,17,34,14,21,13,9
 echo "bootstrap_1602.txt"
-python main.py -c entradas/bootstrap_1602.txt 4,5,17,20,23,28,3,6,19,21
+python main.py -c entradas/bootstrap_1602.txt 20,3,5,6,4,19,17,23,28,21
 echo "bootstrap_1997.txt"
-python main.py -c entradas/bootstrap_1997.txt 2,3,5,10,19,20,21,22,23,24
+python main.py -c entradas/bootstrap_1997.txt 2,19,3,20,21,23,24,5,22,10
 echo "Fim teste"
 echo
 echo
 
-#Rscript spearman_kendall_angular.R
-#Rscript spearman_kendall_jquery.R
-#Rscript spearman_kendall_bootstrap.R
+Rscript spearman_angular.R
+Rscript spearman_jquery.R
+Rscript spearman_bootstrap.R
